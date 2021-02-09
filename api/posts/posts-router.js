c5005e93e0ad879384bf6069e96120cbb127dc15
John Chamberlin <johnchamberlin00@gmail.com>
2021-02-09 14:24:45 -0600

    mvp done, endpoints and routes established

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,108 @@
 // implement your posts router here
+const express = require('express')
+const router = express.Router()
+const Post = require('./posts-model')
+
+router.get('/', (req, res) => {
+  Post.find()
+    .then(post => {
+      res.status(200).json(post)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  Post.findById(id)
+    .then(post => {
+      if (!post) {
+        res.status(404).json({ message: 'No post found with given ID' })
+      } else {
+        res.status(200).json(post)
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+router.post('/', (req, res) => {
+  const { title, contents } = req.body
+  if (!title || !contents) {
+    res.status(404).json({ message: 'Please include title and content' })
+  } else {
+    Post.insert({ title, contents })
+      .then(post => {
+        Post.findById(post.id).then(post => {
+          res.status(201).json(post)
+        })
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { title, contents } = req.body
+  if (!title || !contents) {
+    res.status(404).json({ message: 'Please include title and content' })
+  } else {
+    Post.update(id, { title, contents })
+      .then(post => {
+        if (!post) {
+          res.status(404).json({ message: 'No post found with given ID' })
+        } else {
+          Post.findById(id).then(post => {
+            res.status(201).json(post)
+          })
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  Post.remove(id)
+    .then(post => {
+      if (!post) {
+        res.status(404).json({ message: 'no post found with given id' })
+      } else {
+        res
+          .status(200)
+          .json({ message: `post with id ${id} deleted successfully` })
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: `failed to delete post` })
+    })
+})
+
+router.get('/:id/comments', (req, res) => {
+  const id = req.params.id
+  Post.findPostComments(id)
+    .then(post => {
+      if (!post) {
+        res.status(404).json({ message: `no post found id ${id}` })
+      } else if (post.length === 0) {
+        res
+          .status(404)
+          .json({ message: `no comments associated with post id ${id}` })
+      } else {
+        res.status(200).json(post)
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: 'there was an error retrieving comments' })
+    })
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require("./api/server");
+const PORT = 1234;
+
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
